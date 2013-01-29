14decc60c67c9b7b6ae2ac2241e3215b14b6a6e1
Juan Lopez-de-Armentia <juanarmentia@gmail.com>
2013-01-29 10:04:05 +0100

    Corrected the function readGraph by triple

--- OtsoduinoServer/OtsoduinoServer.ino
+++ OtsoduinoServer/OtsoduinoServer.ino
@@ -1,19 +1,3 @@
-/*
-  Web Server
- 
- A simple web server that shows the value of the analog input pins.
- using an Arduino Wiznet Ethernet shield. 
- 
- Circuit:
- * Ethernet shield attached to pins 10, 11, 12, 13
- * Analog inputs attached to pins A0 through A5 (optional)
- 
- created 18 Dec 2009
- by David A. Mellis
- modified 4 Sep 2010
- by Tom Igoe
- 
- */
 
 #include <SPI.h>
 #include <Ethernet.h>
@@ -23,39 +7,43 @@
 
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
-//byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x79, 0xC1 };
-//IPAddress ip(192,168,59, 177);
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x00, 0x79, 0xC1 };
+IPAddress ip(192,168,59, 177);
+char* myIP = "http://192.168.59.177/";
 
 // Initialize the Ethernet server library
 // with the IP address and port you want to use 
 // (port 80 is default for HTTP):
-//EthernetServer server(80);
+EthernetServer server(80);
 
 File spacesFile;
 
 unsigned long startTime;                      //Momento en el que inicia el programa
 unsigned long timesCounter = 0;               //Número de veces que se ha ejecutado el Write para cálcular cuando se tiene que ejecutar de nuevo
-unsigned int interval = 3000;                   //Intervalo de tiempo para que se ejecute el write
+unsigned int interval = 30000;                 //Intervalo de tiempo para que se ejecute el write
 boolean firstWrite = true;                    //Para controlar si es la primera tripleta de un grafo o son las siguientes las que se escriben
-unsigned int iURL_part2 = 0;                   //Segunda parte de la uri del grafo --> Random(100-999)
-char URL_part2[30];
+char URL_part2[30];							  //Segunda parte de la uri del grafo --> (1000.txt, 1001.txt ...)
 
-boolean firstFile = true;
-char directory[30];
+boolean firstFile = true;					  //Boolean para controlar los accesos a los diferentes directorios en la función searchDirectory, de la función readGraph()
+char directory[30];							  //Variable que se utiliza para almacenar ruta del archivo que se está leyendo en la función searchPath, de la función readGraph()
+char directorySpace[30];							  //Variable que se utiliza para almacenar la ruta del espacio que se está leyendo en la función searchPath, de la función readGraph()
 
 char c;                                       //Carácter en el que se almacena el carácter leído de fichero
-unsigned int indexSpace = EEPROM_readint(0);  //Se guarda el índice del espacio para guardar en spaces.txt (space1000, space1001...)
+unsigned int indexSpace = 0;				  //Se guarda el índice del espacio para guardar en spaces.txt (space1000, space1001...)
+unsigned int indexGraph = 0;				  //Se guarda el índice de los grafos (1000, 1001...)
 
 
 boolean selectedDirectory = false;
 char selectedPath[64];
 char uriGraph[64];
+char graph[2500];
 
 //CONSTANTS
-char* uriSpaceConst		= "http://otsopack3";
+char* uriSpaceConst1	= "http://otsopack1";
 char* subjectConst1		= "subject1";
 char* predicateConst1	= "predicate1";
 char* objectConst1		= "object1";
+char* uriSpaceConst2	= "http://otsopack2";
 char* subjectConst2		= "subject2";
 char* predicateConst2	= "predicate2";
 char* objectConst2		= "object2";
@@ -72,386 +60,78 @@ void setup()
 {
   delay(2000);
   Serial.begin(115200);
-  //Serial.print("EEPROM: ");
-  //Serial.println(indexSpace);
-  //Serial.print("Initializing SD card...");
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output
-  // or the SD library functions will not work.
-  pinMode(53, OUTPUT);
-  while (!SD.begin(53)) {
+
+  pinMode(4, OUTPUT);
+  while (!SD.begin(4)) {
     Serial.println("initialization failed!");
-    //return;
   }
-  //Serial.println("initialization done.");
-  
-  
+
   // start the Ethernet connection and the server:
-//  Ethernet.begin(mac, ip);
-//  server.begin();
-  //Serial.println(Ethernet.localIP());
-  
+  Ethernet.begin(mac, ip);
+  server.begin();
+  //readGraph(uriSpaceConst1,"1107");
+  readGraph(uriSpaceConst1, subjectConst2, predicateConst2, objectConst2);
   startTime = millis();
+  Serial.print("millis(): ");
+  Serial.println(startTime);
 }
 
 void loop()
 {
-	if (millis()/1000>(startTime/1000+(interval*timesCounter))){
-	timesCounter++;
-	//    Serial.println(startTime);
-	//    Serial.println(millis());
-	Serial.println();
-    
-	//Serial.println(readGraph(uriSpaceConst, subjectConst2, predicateConst2, objectConst2));
+	/*if (millis()/1000>(startTime/1000+(interval*timesCounter))){
+		timesCounter++;
+		Serial.println("hola");
+		//Serial.println(readGraph(uriSpaceConst1, subjectConst2, predicateConst2, objectConst2));
 
-	
-	Serial.println(writeTriple(uriSpaceConst, subjectConst1, predicateConst1, objectConst1));
-	Serial.println();
-	delay(1000);
-	
-	Serial.println(writeTriple(uriSpaceConst, subjectConst2, predicateConst2, objectConst2));
-	delay(300);
-	firstWrite = true;
-	delay(1000);
-	    
-	//writeTriple("http://otsopack2", "subject1", "predicate1", "object1");
-	//Serial.println();
-	//Serial.println();
-	//writeTriple("http://otsopack2", "subject3", "predicate3", "object3", firstWrite);
-	//Serial.println();  
-	//firstWrite = true;
-	//delay(1000);
-	}
+		//Serial.println(writeTriple(uriSpaceConst1, subjectConst1, predicateConst1, objectConst1, firstWrite));
+		//Serial.println();
+		//delay(1000);
+
+		//Serial.println(writeTriple(uriSpaceConst1, subjectConst2, predicateConst2, objectConst2, firstWrite));
+		//Serial.println();
+		//firstWrite = true;
+		//delay(1000);
+	 //   
+		//Serial.println(writeTriple(uriSpaceConst1, subjectConst1, predicateConst1, objectConst1, firstWrite));
+		//Serial.println();
+		//firstWrite = true;
+		//delay(1000);
+		//
+		//Serial.println(writeTriple(uriSpaceConst2, subjectConst2, predicateConst2, objectConst2, firstWrite));
+		//Serial.println();  
+		//firstWrite = true;
+		//delay(1000);
+	}*/
   
 	//httpServer();
   
-  
 }
 
 
-//void httpServer(){
-//  // listen for incoming clients
-//  EthernetClient client = server.available();
-//  if (client) {
-//    // an http request ends with a blank line
-//    boolean currentLineIsBlank = true;
-//    while (client.connected()) {
-//      if (client.available()) {
-//        char c = client.read();
-//        // if you've gotten to the end of the line (received a newline
-//        // character) and the line is blank, the http request has ended,
-//        // so you can send a reply
-//        if (c == '\n' && currentLineIsBlank) {
-//          
-//          //Aquí hay que enviar lo que recibamos en el readGraph(). Tratar el string y enviarlo
-//          
-//          // send a standard http response header
-//          client.println("HTTP/1.1 200 OK");
-//          client.println("Content-Type: text/html");
-//          client.println();
-//
-//          // output the value of each analog input pin
-//          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-//            client.print("analog input ");
-//            client.print(analogChannel);
-//            client.print(" is ");
-//            client.print(analogRead(analogChannel));
-//            client.println("<br />");
-//          }
-//          break;
-//        }
-//        if (c == '\n') {
-//          // you're starting a new line
-//          currentLineIsBlank = true;
-//        } 
-//        else if (c != '\r') {
-//          // you've gotten a character on the current line
-//          currentLineIsBlank = false;
-//        }
-//      }
-//    }
-//    // give the web browser time to receive the data
-//    delay(1);
-//    // close the connection:
-//    client.stop();
-//  }
+
+
+//void PrintFile(File myFile, char* myFileName, char* directory){
+//	if (strcmp(directory, "\0")!= 0){
+//		strcat(directory,myFileName);
+//		strcpy(myFileName,directory);
+//	}
+//	// re-open the file for reading:
+//	myFile = SD.open(myFileName);
+//	if (myFile) {
+//		Serial.println("**********************************");
+//		Serial.println(myFileName);
+//  
+//		// read from the file until there's nothing else in it:
+//		while (myFile.available()) {
+//			Serial.write(myFile.read());
+//		}
+//		// close the file:
+//		myFile.close();
+//	} else {
+//		// if the file didn't open, print an error:
+//		Serial.print("Error opening ");
+//		Serial.println(myFileName);
+//	}
+//	Serial.println("-------------------------------------");
 //}
 
-
-
-char* writeTriple(char* space, char* subject, char* predicate, char* object){
-  
-	File graphFile;
-	Serial.print("1. Dentro de writeTriple: ");
-	Serial.println(indexSpace);
-	char spaceUri[64];    //URI del espacio
-	memset(spaceUri, '\0', 64);      
-	char spaceFolder[30];    //Nombre de la carpeta correspondiente al espacio
-	memset(spaceFolder, '\0', 30);
-  
-  
-	//Se empiela a leer spaces.txt para ver si existe el espacio
-	graphFile = SD.open(spacesIndexConst); //No utilizar nombres largos de fichero (formato 8.3)
-	strcpy(spaceUri, readSpaceUri(graphFile));
-	Serial.print("1.5. spaceUri: ");
-	Serial.print(spaceUri);
-	Serial.print("|");
-	Serial.println(space);
-  
-	//Si la URI leída en el fichero no es la misma que hemos pasado a la función o el fichero no está vacío, se sigue leyendo.
-	while((strcmp(spaceUri, space)!=0)&&(strcmp(spaceUri, nothingConst)!=0)){
-	while (c != '\n'){
-		c = graphFile.read();
-	}
-	memset(spaceUri, '\0', 64);
-	strcpy(spaceUri, readSpaceUri(graphFile));
-	Serial.print("1.75. spaceUri: ");
-	Serial.println(spaceUri);
-	}
-  
-	//Si ha salido del while anterior y el fichero no está vacío, es que el espacio que se ha pasado a la función existe en el fichero y leemos el nombre de su carpeta asociada.
-	if (strcmp(spaceUri, nothingConst)!=0){
-	//Leer la parte del nombre de la carpeta
-	memset(spaceFolder, '\0', 30);
-	strcpy(spaceFolder, readSpaceFolder(graphFile));
-	Serial.print("2. spaceFolder: ");
-	Serial.println(spaceFolder);
-	}
-	graphFile.close();
-  
-	Serial.print("3. spaceUri(Nothing): ");
-	Serial.println(spaceUri);
-	//Si no se ha encontrado el espacio en el fichero, se introduce en el spaces.txt y se crea una carpeta
-	if (strcmp(spaceUri, nothingConst)==0){ 
-	Serial.println("4. spaceUri es 'nothing'");
-	spacesFile = SD.open(spacesIndexConst, FILE_WRITE);
-	if (spacesFile) { 
-		spacesFile.print(space);
-		spacesFile.print(sepConst);
-		spacesFile.println(indexSpace); 
-		spacesFile.close();
-	}
-	//Se crea la carpeta
-    
-	memset(spaceFolder, '\0', 30);
-	Serial.print(spaceFolder);
-	itoa(indexSpace, spaceFolder, 10);
-	Serial.print("5. spaceFolder: ");
-	Serial.println(spaceFolder);
-	SD.mkdir(spaceFolder);
-	indexSpace++;
-	EEPROM_writeint(0, indexSpace);
-	}
-  
-	//Guardar en la SD en el directorio SD:\space_uri\uri.txt las tripletas "s;p;o"
-  
-	//Serial.println(firstWrite);
-	if (firstWrite){
-	//Se crea la URI para el nuevo conjunto de tripletas y se comprueba si existe
-	memset(URL_part2, '\0', 30);
-	iURL_part2 = random(100,999);
-	itoa(iURL_part2, URL_part2, 10);
-	}
-    
-	char uriGraph[64];
-	memset(uriGraph, '\0', 64);
-	strcat(uriGraph, spaceFolder);
-	strcat(uriGraph, sepUrlConst);
-	strcat(uriGraph, URL_part2);
-	strcat(uriGraph, extConst);
-	Serial.print("1uriGraph: ");
-	Serial.println(uriGraph);
-  
-	//Serial.print(SD.exists(uriGraph));
-	//Serial.println("II");
-	//Serial.print(firstWrite);
-	//Serial.println("II");
-  
-	while((SD.exists(uriGraph))&&(firstWrite)){
-	Serial.println("dentro del while");
-	memset(URL_part2, '\0', 30);
-	iURL_part2 = random(100,999);
-	itoa(iURL_part2, URL_part2, 10);
-	Serial.println(URL_part2);
-	memset(uriGraph, '\0', 64);
-	strcat(uriGraph, spaceFolder);
-	strcat(uriGraph, sepUrlConst);
-	strcat(uriGraph, URL_part2);
-	strcat(uriGraph, extConst);
-	Serial.print("2uriGraph: ");
-	Serial.print(uriGraph);
-	}
-
-	//firstWrite = false;
-	delay(3000);
-  
-	// open a new file and immediately close it:
-	Serial.print("7. Creating ");
-	delay(3000);
-	char uriGraphcpy[64];
-	memset(uriGraphcpy, '\0', 64);
-	char * pch;
-	pch = strtok (uriGraph,"\r");
-	strcat(uriGraphcpy, pch);
-	while (pch != NULL)
-	{
-		pch = strtok (NULL, "\n");
-		strcat(uriGraphcpy, pch);
-	}
-	Serial.println(uriGraphcpy);
-	graphFile = SD.open(uriGraphcpy, FILE_WRITE); //No utilizar nombres largos de fichero
-	if (graphFile){
-	Serial.println(subject);
-	graphFile.print(subject);
-	graphFile.print('|');
-	graphFile.print(predicate);
-	graphFile.print('|');
-	graphFile.print(object);
-	graphFile.println('|');
-	graphFile.close();
-	}
-
-	char uriGraphReturn[64];
-	memset(uriGraphReturn, '\0', 64);
-	strcat(uriGraphReturn, space);
-	strcat(uriGraphReturn, sepUrlConst);
-	strcat(uriGraphReturn, strtok(uriGraphcpy, ".")); 
-	  
-	return uriGraphReturn;
-}
-
-char* readGraph(char* space, char* subject, char* predicate, char* object){
-  
-	//Hay que recorrer todos los ficheros y carpetas en busca del patrón s,p,o. Cuando se encuentre, se devolverá un String
-	//con todas las líneas del fichero en el que se encuentra el patrón concatenadas (String = "uri|s;p;o|s;p;o|s;p;o");
- 
-	//Utilizar openNextFile() y isDirectory(). Con openNextFile() se recorren los ficheros, cada iteración se comprueba si es directorio con isDirectory().
-	//Si es directorio se guarda el nombre en una variable space_uri o uri. Y si es fichero, se guarda su nombre y se parsea en busca del patrón. 
-	//Cuando se encuentre el patrón, se lee de nuevo ese fichero que tendrá la ruta siguiente (SD:\space_uri\uri\nombre_fichero.txt) y cada tripleta se mete en un String respuesta = "uri|s;p;o|s;p;o|s;p;o".
-	
-	memset(selectedPath, '\0', 64);
-	memset(uriGraph, '\0', 64);
-
-	File fileSpaces = SD.open("SPACES.TXT");
-	while(strcmp(readSpaceUri(fileSpaces),space)!=0){
-		readSpaceFolder(fileSpaces);
-	}
-	//Serial.print("Folder: ");
-	//Serial.println(readSpaceFolder(fileSpaces));
-	char folder[30];
-	memset(folder, '\0', 30);
-	strcpy(folder, readSpaceFolder(fileSpaces));
-	fileSpaces.close();
-	
-	File root = SD.open("/");
-
-	searchPath(root, 0, true, folder, subject, predicate, object, space);
-	Serial.println(selectedPath);
-	//Serial.println(uriGraph);
-
-	root.close();
-	
-	return uriGraph;
-}
-
-
-void searchPath(File dir, int numTabs, boolean firstFile, char* folder, char* subject, char*predicate, char* object, char* space) {
-    
-	while(true) {
-
-		if(firstFile){
-			memset(directory, '\0', 30);
-		}
-		File entry =  dir.openNextFile();
-		if (! entry) {
-			//Serial.println("**nomorefiles**");
-			break;
-		}
-		if (entry.isDirectory()) {
-			if(atoi(entry.name()) == atoi(folder)){
-				strcpy(directory, entry.name());
-				strcat(directory, "/");
-				selectedDirectory = true;
-			}
-			else
-			{
-				selectedDirectory = false;
-			}
-			firstFile = false;
-			searchPath(entry, numTabs+1, firstFile, folder, subject, predicate, object, space);
-		} else if(selectedDirectory) {
-				File selectedFile = SD.open(strcat(directory,entry.name()));
-				if (strcmp(readSpaceUri(selectedFile),subject) == 0)
-					if(strcmp(readSpaceUri(selectedFile),predicate) == 0)
-						if(strcmp(readSpaceUri(selectedFile),object) == 0){
-							strcpy(selectedPath, directory);
-							strcat(uriGraph, space);
-							strcat(uriGraph, "/");
-							strcat(uriGraph, strtok(directory, "."));
-							exit;
-						}
-		}
-	}
-}
-
-void PrintFile(File myFile, char* myFileName, char* directory){
-
-	if (strcmp(directory, "\0")!= 0){
-		strcat(directory,myFileName);
-		strcpy(myFileName,directory);
-
-	}
-	// re-open the file for reading:
-	myFile = SD.open(myFileName);
-	if (myFile) {
-		Serial.println("**********************************");
-		Serial.println(myFileName);
-  
-		// read from the file until there's nothing else in it:
-		while (myFile.available()) {
-			Serial.write(myFile.read());
-		}
-		// close the file:
-		myFile.close();
-	} else {
-		// if the file didn't open, print an error:
-		Serial.print("Error opening ");
-		Serial.println(myFileName);
-	}
-	Serial.println("-------------------------------------");
-}
-
-//Read the side where space`s URI is written
-char* readSpaceUri(File gFile){
-  char rSpaceUri[64];
-  memset(rSpaceUri, '\0', 64);
-  int index = 0;
-  char c = gFile.read();
-  if (c!=-1){
-    while ((c != '|')){     
-      rSpaceUri[index++] = c;
-      c = gFile.read();      
-    }
-    delay(200);
-  }
-  else{
-    return "nothing"; 
-  }
-  return rSpaceUri;
-}
-
-//Read the side where the folder, relationated with the space's URI, is written
-char* readSpaceFolder(File gFile){
-  char rSpaceFolder[64];
-  memset(rSpaceFolder, '\0', 64);
-  int index = 0;
-  char c = gFile.read();
-  while ((c != '\n')&&(c != ' ')){
-    
-    rSpaceFolder[index++] = c;
-    delay(200);
-    c = gFile.read();
-  }
-  return rSpaceFolder;
-}
---
+++ OtsoduinoServer/ethernetFuncs.ino
@@ -0,0 +1,268 @@
+void httpServer(){
+	// listen for incoming clients
+	EthernetClient client = server.available();
+	char url[128];
+	memset(url, '\0', 128);
+	
+	char* get = "GET ";
+	char* space = "/spaces/";
+	char* graph = "/graph/";
+
+	int option = 0;
+	int i=0;
+	boolean urlArrived = false;
+	int contBlanks = 0;
+	int contSep = 0;
+	
+	char bufferSpace[15];
+	memset(bufferSpace, '\0', 15);
+	char bufferGraph[15];
+	memset(bufferGraph, '\0', 15);
+	char bufferWildcards[15];
+	memset(bufferWildcards, '\0', 15);
+	char bufferSpaceVar[64];
+	memset(bufferSpaceVar, '\0', 64);
+	char bufferGraphVar[15];
+	memset(bufferGraphVar, '\0', 15);
+	char bufferSubjectVar[64];
+	memset(bufferSubjectVar, '\0', 64);
+	char bufferPredicateVar[64];
+	memset(bufferPredicateVar, '\0', 64);
+	char bufferObjectVar[64];
+	memset(bufferObjectVar, '\0', 64);
+	
+	int indexBuffer = 0;
+	boolean isGraphVar = true;
+	char* identifyGet = "000";
+
+	if (client) {
+	// an http request ends with a blank line
+	boolean currentLineIsBlank = true;
+	while (client.connected()) {
+		if (client.available()) {
+		char c = client.read();
+		//client.print(c);
+		// if you've gotten to the end of the line (received a newline
+		// character) and the line is blank, the http request has ended,
+		// so you can send a reply
+
+		if(urlArrived){
+			url[i] = c;
+			i++;
+		}
+		
+		if (c == ' ' && contBlanks < 2) {
+			contBlanks++;
+			if (urlArrived == true) {
+				urlArrived =false;
+			}
+			else urlArrived = true;
+		}
+		
+		
+		
+		
+
+		if (c == '\n' && currentLineIsBlank) {
+			
+			url[i-1]='\0';
+			client.print("*");
+			client.print(url);
+			client.println("*");
+			client.println(strlen(url));
+			for(int j=0; j < strlen(url); j++){
+
+				if (url[j] == '/'){
+					contSep++;
+					indexBuffer = 0;
+				}
+				if (url[j] != '/'){
+
+					switch (contSep)
+					{
+						case 1:{ //"spaces"
+							bufferSpace[indexBuffer] = url[j];
+							indexBuffer++;
+						}break;
+						case 2:{ //spaceUrl
+							bufferSpaceVar[indexBuffer++] = url[j];
+						}break;
+						case 3:{ //"graph" or "graphs"
+							identifyGet[0] = '1';
+							bufferGraph[indexBuffer++] = url[j];
+						}break;
+						case 4:{ //graphNumber or "wildcards"
+							if (isGraphVar){
+								if (url[j] == 'w' || url[j] == 'W'){
+									isGraphVar = false;
+									bufferWildcards[indexBuffer++] = url[j];
+									identifyGet[2] = '1';
+								}
+								else{
+									bufferGraphVar[indexBuffer++] = url[j];
+									identifyGet[1] = '1';
+								}
+							}
+							else
+							{
+								bufferWildcards[indexBuffer++] = url[j];
+							}
+						
+						}break;
+						case 5:{ //Subject
+							bufferSubjectVar[indexBuffer++] = url[j];
+						}break;
+						case 6:{ //Predicate
+							bufferPredicateVar[indexBuffer++] = url[j];
+						}break;
+						case 7:{ //Object
+							bufferObjectVar[indexBuffer++] = url[j];
+						}break;
+						default:{
+							client.println("Default");
+						}break;
+					}
+				}
+			}
+ 
+			//Aquí hay que enviar lo que recibamos en el readGraph(). Tratar el string y enviarlo
+          
+			// send a standard http response header
+			//client.println("HTTP/1.1 200 OK");
+			//client.println("Content-Type: text/html");
+			client.print("url: ");
+			client.println(url);
+			//client.println("<br />");
+			client.print("bufferSpace: ");
+			client.println(bufferSpace);
+			//client.println("<br />");
+			client.print("bufferSpaceVar: ");
+			client.println(bufferSpaceVar);
+			//client.println("<br />");
+			client.print("bufferGraph: ");
+			client.println(bufferGraph);
+			//client.println("<br />");
+			client.print("bufferGraphVar: ");
+			client.println(bufferGraphVar);
+
+			if (contSep>4){
+				client.print("bufferWildcards: ");
+				client.println(bufferWildcards);
+				//client.println("<br />");
+				client.print("bufferSubjectVar: ");
+			    client.println(bufferSubjectVar);
+				//client.println("<br />");
+				client.print("bufferPredicateVar: ");
+			    client.println(bufferPredicateVar);
+				//client.println("<br />");
+				client.print("bufferObjectsVar: ");
+			    client.println(bufferObjectVar);
+				//client.println("<br />");
+			}
+
+			contSep = 0;
+			isGraphVar = true;
+
+			if(strcmp(identifyGet, "110")==0){
+				client.println("110 dentro");
+				client.println(urldecode(bufferSpaceVar));
+
+				client.println(readGraph(urldecode(bufferSpaceVar),"1107"));
+			}
+			if(strcmp(identifyGet, "111")==0){
+				client.println("111 dentro");
+				client.println("urldecode(uriSpaceConst1)");
+
+				client.println(readGraph(urldecode(bufferSpaceVar), bufferSubjectVar, bufferPredicateVar, urldecode(bufferObjectVar)));
+			}
+
+			// output the value of each analog input pin
+			//for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+			//	client.print("analog input ");
+			//	client.print(analogChannel);
+			//	client.print(" is ");
+			//	client.print(analogRead(analogChannel));
+			//	client.println("<br />");
+			//}
+			break;
+			memset(bufferSpace, '\0', 15);
+			memset(bufferGraph, '\0', 15);
+			memset(bufferWildcards, '\0', 15);
+			memset(bufferSpaceVar, '\0', 15);
+			memset(bufferGraphVar, '\0', 15);
+			memset(bufferSubjectVar, '\0', 64);
+			memset(bufferPredicateVar, '\0', 64);
+			memset(bufferObjectVar, '\0', 64);
+			identifyGet[0] = '0';
+			identifyGet[1] = '0';
+			identifyGet[2] = '0';
+		}
+		if (c == '\n') {
+			// you're starting a new line
+			currentLineIsBlank = true;
+		} 
+		else if (c != '\r') {
+			// you've gotten a character on the current line
+			currentLineIsBlank = false;
+		}
+		}
+	}
+	// give the web browser time to receive the data
+	delay(1);
+	// close the connection:
+	client.stop();
+	}
+}
+
+
+// http://  to  http%3a%2f%2f
+char* urlencode(const char* msg)
+{
+    const char *hex = "0123456789abcdef";
+    String sEncodedMsg = "";
+
+    while (*msg!='\0'){
+        if( ('a' <= *msg && *msg <= 'z')
+                || ('A' <= *msg && *msg <= 'Z')
+                || ('0' <= *msg && *msg <= '9') ) {
+            sEncodedMsg += *msg;
+        } else {
+            sEncodedMsg += '%';
+            sEncodedMsg += hex[*msg >> 4];
+            sEncodedMsg += hex[*msg & 15];
+        }
+        msg++;
+    }
+    
+    char encodedMsg[80];
+    memset(encodedMsg, '\0', 80);
+    sEncodedMsg.toCharArray(encodedMsg,80);
+    return encodedMsg;
+}
+
+// http%3a%2f%2f  to  http://
+char* urldecode(char* url) {
+	char dest[128];
+	memset(dest, '\0', 128);
+	char buffer[4];
+    memset(buffer, '\0', 4);
+	int i;
+	int contSpecialChar = 0;
+
+	for(i = 0; i < strlen(url); i++) {
+		for(int j = 1; j < 3; j++){
+			buffer[j-1] = buffer[j];
+		}
+		buffer[2] = url[i];
+		dest[i-2*contSpecialChar] = url[i];
+		if(strcmp(buffer, "%3A")==0  || strcmp(buffer, "%3a")==0){
+			contSpecialChar++;
+			dest[i-2*contSpecialChar] = ':';		
+		}
+		if(strcmp(buffer, "%2F")==0 || strcmp(buffer, "%2f")==0){
+			contSpecialChar++;
+			dest[i-2*contSpecialChar] = '/';			
+		}
+	}
+	return dest;
+}
