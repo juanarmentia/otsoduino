7b3be1893c1c00d1717e3c17365c71c86fdb2262
Juan Lopez-de-Armentia <juanarmentia@gmail.com>
2013-05-14 16:28:34 +0200

    Structure of files tidied up

--- OtsoduinoServer/memoryFunctions.ino
+++
@@ -1,9 +0,0 @@
-void printpgm(char *texto)
-{
-  char *mem=texto;
-
-  while (pgm_read_byte(mem) != 0x00) /* Comparamos con \0, un terminador */
-    Serial.print(pgm_read_byte(mem++));
-  Serial.println();
-}
-
